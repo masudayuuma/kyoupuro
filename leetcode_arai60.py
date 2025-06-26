24cb381c396d3b47295a825f33e209155e5caa63
masudayuuma <ri0155sk@ed.ritsumei.ac.jp>
2025-06-26 11:32:37 +0900

    0626_coding

--- leetcode_arai60.py
+++ leetcode_arai60.py
@@ -1,4 +1,4 @@
-#Linked List Cycle
+#Linked List Cycle　これでも行けるが最適ではないらしい
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -16,5 +16,66 @@
 #             current_node = current_node.next
 #         return False
 
+#Linked List Cycle II これでも行けるが最適ではないらしい
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+# class Solution:
+#     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         visited_nodes = set()
+#         current_node = head
+#         while current_node:
+#             if current_node in visited_nodes:
+#                 return current_node
+#             visited_nodes.add(current_node)
+#             current_node = current_node.next
+#         return None
     
+#Linked List Cycle
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         fast = head
+#         slow = head
+#         while fast and fast.next:
+#             fast = fast.next.next
+#             slow = slow.next
+
+#             if fast == slow:
+#                 return True
+#         return False
+
+#Linked List Cycle II
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast =head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break
+        else: return None
+
+        fast = head
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+
+        return slow
