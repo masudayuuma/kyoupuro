6d0402ec920d497101181e2c472578e74e6eab13
masudayuuma <ri0155sk@ed.ritsumei.ac.jp>
2025-06-30 23:19:09 +0900

    0630_coding

--- leetcode_arai60.py
+++ leetcode_arai60.py
@@ -163,3 +163,35 @@
 #         return res.next
     
 
+#Valid Parentheses
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         mapping = {")":"(", "}":"{", "]": "["}
+
+#         for char in s:
+#             if char in mapping.values():
+#                 stack.append(char)
+#             elif char in mapping.keys():
+#                 if not stack or mapping[char] != stack.pop():
+#                     return False
+                
+#         return not stack
+    
+#Reverse Linked List
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = None
+        while head:
+            temp = head.next
+            head.next = node
+            node = head
+            head = temp
+
+        return node
+    
