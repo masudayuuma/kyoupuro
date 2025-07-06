c2152b68eeab75ea28af26659d9ce29d28136a00
masudayuuma <ri0155sk@ed.ritsumei.ac.jp>
2025-07-06 13:22:11 +0900

    0706_coding

--- leetcode_arai60.py
+++ leetcode_arai60.py
@@ -299,3 +299,15 @@
 #             if complement in hashmap and hashmap[complement] != i:
 #                 return [i, hashmap[complement]]
 #         return []
+
+#Group Anagrams
+# from collections import defaultdict
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         groups = defaultdict(list)
+#         for str in strs:
+#             groups[tuple(sorted(str))].append(str)
+#         return list(groups.values())
+
+
