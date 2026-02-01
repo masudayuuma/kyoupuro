f9672d1b73cfb9c296362d548a32fa10104787f1
masudayuuma <haruouma0528@docomo.ne.jp>
2026-02-01 19:30:28 +0900

    0201_coding

--- Novistep/queue.py
+++ Novistep/queue.py
@@ -14,15 +14,37 @@
 #         que.popleft()
 
 # B - Restaurant Queue
+# from collections import deque
+
+# Q = int(input())
+# que = deque()
+# for _ in range(Q):
+#     query = input().split()
+
+#     if query[0] == '1':
+#         que.append(query[1])
+#     else:
+#         num = que.popleft()
+#         print(num)
+
+# B52 - Ball Simulation
 from collections import deque
 
-Q = int(input())
-que = deque()
-for _ in range(Q):
-    query = input().split()
+N, X = map(int, input().split())
+A = list(input())
 
-    if query[0] == '1':
-        que.append(query[1])
-    else:
-        num = que.popleft()
-        print(num)
+A[X-1] = '@'
+
+que = deque()
+que.append(X-1)
+
+while len(que) > 0:
+    t = que.popleft()
+    if t < N-1 and A[t+1] == '.':
+        que.append(t+1)
+        A[t+1] = '@'
+    if t > 0 and A[t-1] == '.':
+        que.append(t-1)
+        A[t-1] = '@'
+
+print(''.join(A))
