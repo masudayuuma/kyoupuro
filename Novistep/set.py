242e207ccf556f301ddd65cf5959fff4d7455f5a
masudayuuma <haruouma0528@docomo.ne.jp>
2026-03-25 20:10:40 +0900

    0325_coding

--- Novistep/set.py
+++ Novistep/set.py
@@ -10,3 +10,16 @@
 # N = int(input())
 # A = list(map(int, input().split()))
 # print(len(set(A)))
+
+# B - Substring
+# S = input()
+# ans_set = set()
+# tmp = ''
+# for i in range(len(S)):
+#     tmp = S[i]
+#     ans_set.add(tmp)
+#     for j in range(i+1, len(S)):
+#         tmp += S[j]
+#         ans_set.add(tmp)
+
+# print(len(ans_set))
