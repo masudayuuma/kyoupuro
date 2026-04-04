51b386300d52ac956633f8c628c5ffe1d81384c7
masudayuuma <haruouma0528@docomo.ne.jp>
2026-04-04 20:51:38 +0900

    0404_coding

--- neetcode150.py
+++ neetcode150.py
@@ -23,9 +23,26 @@
 #             return False
 
 # Two Sum
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)-1):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i]+nums[j] == target:
+#                     return [i, j]
+
+# Group Anagrams
+# from collections import defaultdict
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         ans = defaultdict(list)
+
+#         for s in strs:
+#             char_list = "".join(sorted(s))
+#             ans[char_list].append(s)
+
+#         final = []
+#         for a in ans.values():
+#             final.append(a)
+
+#         return final
+    
