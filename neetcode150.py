3291786e301ce87ad2864291b06c0f5d2f86346e
masudayuuma <haruouma0528@docomo.ne.jp>
2026-04-15 15:08:48 +0900

    0415_coding

--- neetcode150.py
+++ neetcode150.py
@@ -108,5 +108,90 @@
 #         return ans
 
 # Valid Sudoku
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+# class Solution:
+#     def isValidSudoku(self, board: List[List[str]]) -> bool:
+#         for row in range(9):
+#             seen = set()
+#             for col in range(9):
+#                 if board[row][col] == '.':
+#                     continue
+#                 if board[row][col] in seen:
+#                     return False
+#                 seen.add(board[row][col])
+
+#         for col in range(9):
+#             seen = set()
+#             for row in range(9):
+#                 if board[row][col] == '.':
+#                         continue
+#                 if board[row][col] in seen:
+#                     return False
+#                 seen.add(board[row][col])
+
+#         for square in range(9):
+#             seen = set()
+#             for i in range(3):      
+#                 for j in range(3):
+#                     row = (square//3)*3+i
+#                     col = (square%3)*3+j
+#                     if board[row][col] == '.':
+#                         continue
+#                     if board[row][col] in seen:
+#                         return False
+#                     seen.add(board[row][col])
+#         return True
+    
+    # [[0, 1, 2],
+    #  [0, 1, 2],
+    #  []]
+
+# Longest Consecutive Sequence
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+#         if nums == []:
+#             return 0 
+
+#         nums = sorted(list((set(nums))))
+#         right, left = 1, 0
+#         count = 1
+#         max_count = 1
+        
+#         while right < len(nums):
+#             if nums[right] == nums[left]+1:
+#                 count += 1
+#                 left += 1
+#             else:
+#                 left = right
+#                 count = 1
+#             right += 1
+#             max_count = max(max_count, count)
+                
+#         return max_count
+            
+# Valid Palindrome
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         string = ""
+#         for c in s:
+#             if c.isalnum():
+#                 string += c.lower()
+
+#         i = 0
+#         while i < len(string)//2:
+#             if string[i] == string[-i-1]:
+#                 i += 1
+#                 continue
+#             else:
+#                 return False
+#         return True
+
+# Two Integer Sum II
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         for i in range(len(numbers)-1):
+#             for j in range(i+1, len(numbers)):
+#                 if numbers[i]+numbers[j] == target:
+#                     return [i, j]
+                
+# 3Sum
+
