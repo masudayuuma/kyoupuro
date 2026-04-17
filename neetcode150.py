71c6f6ad59be7db4230950ce063529d4ff9a98b9
masudayuuma <haruouma0528@docomo.ne.jp>
2026-04-17 17:32:50 +0900

    0417_coding

--- neetcode150.py
+++ neetcode150.py
@@ -193,5 +193,217 @@
 #                 if numbers[i]+numbers[j] == target:
 #                     return [i, j]
                 
-# 3Sum
+# 正解
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         right = len(numbers)-1
+#         left = 0
 
+#         while left < right:
+#             if numbers[left]+numbers[right] > target:
+#                 right -= 1
+#                 continue
+
+#             if numbers[left]+numbers[right] < target:
+#                 left += 1
+#                 continue
+
+#             if numbers[left]+numbers[right] == target:
+#                 return [left+1, right+1]
+            
+
+# 3Sum
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         from collections import defaultdict
+#         nums = sorted(nums)
+#         numsdict = defaultdict(int)
+#         ans = set()
+#         for n in nums:
+#             numsdict[n] += 1
+
+#         l, r = 0, len(nums)-1
+#         while l < r:
+#             if nums[l] > nums[l]+nums[r]:
+#                 l += 1
+#             if nums[r] < nums[l]+nums[r]:
+#                 r += 1
+#             numsdict[nums[l]] -= 1
+#             numsdict[nums[r]] -= 1
+#             if numsdict[-(nums[l]+nums[r])] > 0:
+#                 tmp = [nums[l], nums[r], -(nums[l]+nums[r])]
+#                 ans.add(tuple(sorted(tmp)))
+#             numsdict[nums[l]] += 1
+#             numsdict[nums[r]] += 1
+            
+        
+#         return ans
+
+
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         nums.sort()
+
+#         for i, a in enumerate(nums):
+#             if i > 0 and a == nums[i-1]:
+#                 continue
+
+#             l, r = i+1, len(nums)-1
+#             while l < r:
+#                 threeSum = a + nums[l] + nums[r]
+#                 if threeSum > 0:
+#                     r -= 1
+#                 elif threeSum < 0:
+#                     l += 1
+#                 else:
+#                     res.append([a, nums[l], nums[r]])
+#                     l += 1
+#                     r -= 1
+#                     while nums[l] == nums[l-1] and l < r:
+#                         l += 1
+#         return res
+
+# Container With Most Water
+# class Solution:
+#     def maxArea(self, heights: List[int]) -> int:
+        
+#         max_area = 0
+#         for left in range(len(heights)-1):
+#             right =len(heights)-1
+#             saiteki = heights[left]*(right-left)
+#             if max_area >= saiteki:
+#                 continue
+#             while left < right:
+#                 if heights[left] <= heights[right]:
+#                     max_area = max(max_area, (right-left)*heights[left])
+#                     break
+#                 else:
+#                     max_area = max(max_area, min(heights[left], heights[right])*(right-left))
+#                     right -= 1
+#         return max_area
+                
+# Trapping Rain Water
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+#         left = 0
+#         right = 0
+#         cnt = 0
+        
+#         while right < len(height)-1:
+#             right += 1
+#             if height[right] < 1:
+#                 continue
+#             wall = min(height[left], height[right])
+#             tmp_cnt = 0
+#             while left < right:
+#                 left += 1
+#                 calc = wall - height[left]
+#                 if calc > 0:
+#                     tmp_cnt += calc
+                
+#             cnt += tmp_cnt
+#         return cnt
+
+
+# Best Time to Buy and Sell Stock
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         minby = 10000
+#         for i in prices:
+#             if minby > i:
+#                 minby = i
+#                 continue
+#             else:
+#                 ans = max(ans, i-minby)
+
+#         return ans
+            
+# Longest Substring Without Repeating Characters
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         c_set = set()
+#         ans = 0
+#         del_index = 0
+#         for c in s:
+#             while c in c_set:
+#                 c_set.remove(s[del_index])
+#                 del_index += 1
+#             c_set.add(c)
+#             ans = max(len(c_set), ans)
+
+#         return ans
+
+# Longest Repeating Character Replacement
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+#         from collections import defaultdict
+#         scnt = defaultdict(int)
+#         l = 0
+#         r = 1
+#         while l < r:
+#             while r < len(s):
+#                 scnt[s[r]] += 1
+#                 if r-l+1 -scnt[s[l]] > k:
+#                     continue
+#                 else:
+#                     break
+
+#             ans
+                
+            
+            
+# Valid Parentheses
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         c_dict = { '(': ')', '[':']', '{':'}'}
+#         stack = []
+
+#         for c in s:
+#             if c in c_dict:
+#                 stack.append(c)
+#             else:
+#                 if stack == []:
+#                     return False
+#                 target = stack.pop()
+#                 if not (target in c_dict and c_dict[target] ==c):
+#                     return False
+#         return True if stack == [] else False
+
+# Evaluate Reverse Polish Notation
+# class Solution:
+#     def evalRPN(self, tokens: List[str]) -> int:
+#         stack = []
+
+#         for t in tokens:
+
+#             if t ==  "+":
+#                 t_1 = stack.pop()
+#                 t_2 = stack.pop() 
+#                 ans = t_1 + t_2
+#                 stack.append(ans)
+#                 continue
+#             if t ==  "-":
+#                 t_1 = stack.pop()
+#                 t_2 = stack.pop() 
+#                 ans = t_1 - t_2
+#                 stack.append(ans)
+#                 continue
+#             if t ==  "*":
+#                 t_1 = stack.pop()
+#                 t_2 = stack.pop() 
+#                 ans = t_1 + t_2
+#                 stack.append(ans)
+#                 continue
+#             if t ==  "/":
+#                 t_1 = stack.pop()
+#                 t_2 = stack.pop() 
+#                 ans = t_1 / t_2
+#                 stack.append(ans)
+#                 continue
+#             else:
+#                 stack.append(int(t))
+        
+#         return stack[0]
+
+# Daily Temperatures
