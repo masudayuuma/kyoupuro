6cdfc1d25b6dc8e45a3246c852f1c3351360c279
masudayuuma <haruouma0528@docomo.ne.jp>
2026-08-24 10:24:58 +0900

    0823_coding

--- 2026/0822_atcoder.py
+++ 2026/0822_atcoder.py
@@ -146,3 +146,57 @@ for _ in range(T):
         print(-1)
                 
 
+# c
+# N, M, K = map(int, input().split())
+# A = list(map(int, input().split()))
+# s = 0
+
+# for i in range(N):
+#     if i >= M:
+#         s -= A[i-M]
+#     if s+A[i] <= K:
+#         s += A[i]
+#         print('Yes')
+#     else:
+#         A[i] = 0
+#         print('No')
+
+# d
+# from collections import deque
+
+# h, w, k = map((int, input().split()))
+# s = [input() for _ in range(h)]
+
+# r = [0]*h
+# c = [0]*w
+# for i in range(h):
+#     for j in range(w):
+#         if s[i][j] == "#":
+#             r[i] = 1
+#             c[i] = 1
+
+# d = [[-1]*w for _ in range(h)]
+# q = deque()
+
+# for i in range(h):
+#     for j in range(w):
+#         if r[i] == c[j] == 0:
+#             d[i][j] = 0
+#             q.append((i, j))
+
+# ans = 0
+# while q:
+#     i, j = q.popleft()
+#     if d[i][j] <= k:
+#         ans += 1
+
+#     for ni, nj in ((i-1, j), (i+1, j), (i, j-1), (i, j+1)):
+#         if 0 <= ni < h and 0 <= nj < w:
+#             if s[ni][nj] == '.' and d[ni][nj] ==-1:
+#                 d[ni][nj] == '.' and d[ni][nj] == -1:
+#                 d[ni][nj] == d[i][j]+1
+#                 q.append((ni, nj))
+
+# print(ans)
+
+#
